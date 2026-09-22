166319e436f373d2a4910aa6abd468552e149b96
mazharul90007 <mazharul90007@gmail.com>
2026-09-22 19:49:26 +0600

    dot net updated

--- dotnet/learning/view_practice/view_practice/Models/Person.cs
+++ dotnet/learning/view_practice/view_practice/Models/Person.cs
@@ -2,7 +2,13 @@ namespace view_practice.Models
 {
     public class Person
     {
-        public string? name {get; set;}
-        public DateTime dateOfBirth{get; set;}
+        public string? name { get; set; }
+        public DateTime? dateOfBirth { get; set; }
+        public Gender personGender { get; set; }
+    }
+
+    public enum Gender
+    {
+        Male, Female, Other
     }
 }
