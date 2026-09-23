933dd7207a7230f4d7c9e6ea5a43813385f736e6
Mazharul Islam Sourabh <mazharul90007@gmail.com>
2026-09-23 14:20:38 +0600

    updated

--- dotnet/learning/view_practice/view_practice/Controllers/HomeController.cs
+++ dotnet/learning/view_practice/view_practice/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 using System.Runtime.Versioning;
 using Microsoft.AspNetCore.Mvc;
+using view_practice.Models;
 
 namespace view_practice.Controllers
 {
@@ -8,7 +9,42 @@ namespace view_practice.Controllers
         [Route("home")]
         public IActionResult Index()
         {
-            return View();
+            ViewData["appTitle"] = "View Practice";
+
+            ViewData["person"] = new Person()
+            {
+                name = "Mazharul Islam",
+                dateOfBirth = null,
+                personGender = Gender.Male
+            };
+
+            var people = new List<Person>
+            {
+                new Person()
+                {
+                    name = "Mazharul Islam",
+                    dateOfBirth = null,
+                    personGender = Gender.Male
+                },
+
+                new Person()
+                {
+                    name = "Nusrat Jahan",
+                    dateOfBirth = new DateTime(2000, 05, 05),
+                    personGender = Gender.Female
+                },
+
+                new Person()
+                {
+                    name = "Rakib Hasan",
+                    dateOfBirth = null,
+                    personGender = Gender.Male
+                }
+            };
+
+            // ViewData["people"] = people;
+            // ViewBag.people = people;
+            return View("Index", people);
         }
     }
 }
